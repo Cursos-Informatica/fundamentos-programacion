b02a755cd007bae75d968d73ed582bd13819f36a
Rodrigo Pizarro <rodrigopizarro083@gmail.com>
2025-03-08 18:16:01 -0500

    Se agrego app_1_1.java y app_1_2.java

---
+++ src/app_1_1.java
@@ -0,0 +1,8 @@
+/**
+* Muestra por pantalla la frase "¡Hola mundo!"
+*/
+public class app_1_1 { // Clase principal
+    public static void main(String[] args) {
+    System.out.println("¡Hola mundo!");
+    }
+    }
---
+++ src/app_1_2.java
@@ -0,0 +1,13 @@
+/**
+* Colorear texto
+*/
+public class app_1_2 {
+    public static void main(String[] args) {
+    System.out.print("\033[33m mandarina");
+    System.out.print("\033[32m pera");
+    System.out.print("\033[31m manzana");
+    System.out.print("\033[37m algodon");
+    System.out.print("\033[35m uva");
+    System.out.print("\033[34m arandano");
+    }
+    }
