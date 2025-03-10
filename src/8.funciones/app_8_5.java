5b0a43e649dd7b9f26e6378fcec44f34ed32e3ef
Rodrigo Pizarro <rodrigopizarro083@gmail.com>
2025-03-10 08:56:20 -0500

    Se agrego session 09 10 11.md y la carpeta clases

---
+++ src/8.funciones/app_8_5.java
@@ -0,0 +1,20 @@
+public class app_8_5 {
+    public static void main(String[] args) {
+        int n[] = {8, 33, 200, 150, 11};
+        int m[] = new int[5];
+        muestraArray(n);
+        incrementa(n);
+        muestraArray(n);
+        }
+        public static void muestraArray(int x[]) {
+        for (int i = 0; i < x.length; i++) {
+        System.out.print(x[i] + " ");
+        }
+        System.out.println();
+        }
+        public static void incrementa(int x[]) {
+        for (int i = 0; i < x.length; i++) {
+        x[i]++;
+        }
+        }
+}
